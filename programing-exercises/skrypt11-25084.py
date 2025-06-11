9350546ac7f448ec8bf5cc7f4bd82cae8ca82ce9
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:20:46 +0200

    Update skrypt11-25084.py

--- programing-exercises/skrypt11-25084.py
+++ programing-exercises/skrypt11-25084.py
@@ -1,5 +1,7 @@
 def reverse_sentence():
+    # Pobieramy od użytkownika zdanie
     tekst = input("Podaj zdanie: ")
+    # Wypisujemy zdanie odwrócone znak po znaku (od końca do początku)
     print("Odwrócone:", tekst[::-1])
 
 if __name__ == '__main__':
