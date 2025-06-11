f43c50698a39212dbe80010bcb8007cf81975706
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:31:05 +0200

    Update skrypt25-25084.py

--- programing-exercises/skrypt25-25084.py
+++ programing-exercises/skrypt25-25084.py
@@ -2,8 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 def pobierz_linki(url):
+    # Wysyłamy żądanie HTTP GET pod podany adres URL
     response = requests.get(url)
+    # Parsujemy pobrany kod HTML strony za pomocą BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
+    # Znajdujemy wszystkie tagi <a> z atrybutem href i zbieramy ich wartości
     linki = [a['href'] for a in soup.find_all('a', href=True)]
     return linki
 
