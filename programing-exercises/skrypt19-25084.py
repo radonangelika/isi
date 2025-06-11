cddb446dcfbc76c379b2a6c0a989409311c671cb
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:25:58 +0200

    Update skrypt19-25084.py

--- programing-exercises/skrypt19-25084.py
+++ programing-exercises/skrypt19-25084.py
@@ -1,5 +1,7 @@
 def is_palindrome():
+    # Pobieramy tekst od użytkownika, usuwamy spacje i zmieniamy na małe litery
     tekst = input("Podaj tekst: ").replace(" ", "").lower()
+    # Sprawdzamy, czy tekst jest taki sam czytany od tyłu
     print("To palindrom." if tekst == tekst[::-1] else "To NIE jest palindrom.")
 
 if __name__ == '__main__':
