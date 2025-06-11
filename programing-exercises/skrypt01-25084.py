2270fd7e084dc8d89e1d1dcfbcf7c3dcf5196076
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:14:16 +0200

    Update skrypt01-25084.py

--- programing-exercises/skrypt01-25084.py
+++ programing-exercises/skrypt01-25084.py
@@ -1,8 +1,13 @@
 def is_digit_check():
+    # Pobieramy od użytkownika znak i bierzemy tylko pierwszy znak (indeks 0)
     znak = input("Podaj znak: ")[0]
 
+    # Sprawdzamy, czy znak jest cyfrą za pomocą metody isdigit()
     print(f"Sposób 1 (isdigit): {znak.isdigit()}")
+
+    # Sprawdzamy, czy znak jest typu string i czy należy do ciągu cyfr '0123456789'
     print(f"Sposób 2 (isinstance + in): {isinstance(znak, str) and znak in '0123456789'}")
 
+# Jeśli ten plik jest uruchamiany bezpośrednio, wywołujemy funkcję is_digit_check()
 if __name__ == '__main__':
     is_digit_check()
