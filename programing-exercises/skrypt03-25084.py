bd8c8ae6e3095542c4704d6e25ae031d10b73b2d
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:15:28 +0200

    Update skrypt03-25084.py

--- programing-exercises/skrypt03-25084.py
+++ programing-exercises/skrypt03-25084.py
@@ -1,6 +1,9 @@
 def find_substring():
+    # Pobieramy od użytkownika tekst, w którym będziemy szukać
     tekst = input("Podaj tekst: ")
+    # Pobieramy ciąg znaków, którego będziemy szukać w tekście
     szukany = input("Podaj ciąg do wyszukania: ")
+    # Metoda find() zwraca indeks pierwszego wystąpienia szukanego ciągu lub -1, jeśli go nie ma
     print(tekst.find(szukany))  # -1 jeśli brak
 
 if __name__ == '__main__':
