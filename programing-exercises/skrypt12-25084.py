2f8e57e940dc56017fcbc363d03b8519b26463d9
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:21:29 +0200

    Update skrypt12-25084.py

--- programing-exercises/skrypt12-25084.py
+++ programing-exercises/skrypt12-25084.py
@@ -1,6 +1,10 @@
 def zamien_znaki():
+    # Pobieramy od użytkownika zdanie
     tekst = input("Podaj zdanie: ")
+    # Zamieniamy litery na cyfry według zadanego wzoru:
+    # 'o' na '0', 'e' na '3', 'i' na '1', 'a' na '4'
     zamienione = tekst.replace("o", "0").replace("e", "3").replace("i", "1").replace("a", "4")
+    # Wypisujemy zmieniony ciąg znaków
     print("Po zamianie:", zamienione)
 
 if __name__ == '__main__':
