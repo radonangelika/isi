6ccc3788c5a26ccc91e2f6e9075a997e89fafe6c
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:28:40 +0200

    Update skrypt23-25084.py

--- programing-exercises/skrypt23-25084.py
+++ programing-exercises/skrypt23-25084.py
@@ -2,14 +2,20 @@ import random
 import string
 
 def generuj_hasla(liczba, dlugosc):
+    # Zbiór znaków: małe i wielkie litery oraz cyfry
     znaki = string.ascii_letters + string.digits
+    # Generujemy listę haseł: każde o podanej długości, liczba haseł zgodna z argumentem
     return [''.join(random.choices(znaki, k=dlugosc)) for _ in range(liczba)]
 
 def zapisz_do_pliku(nazwa_pliku, hasla):
+    # Otwieramy plik do zapisu (kodowanie UTF-8)
     with open(nazwa_pliku, 'w', encoding='utf-8') as f:
+        # Zapisujemy każde hasło w osobnej linii
         for haslo in hasla:
             f.write(haslo + '\n')
 
 if __name__ == '__main__':
+    # Generujemy 1000 haseł, każde o długości 6 znaków
     hasla = generuj_hasla(1000, 6)
+    # Zapisujemy wygenerowane hasła do pliku 'passwords.txt'
     zapisz_do_pliku('passwords.txt', hasla)
