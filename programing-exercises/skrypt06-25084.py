bf034794a40133ec4935c382033b4b9e0fd98ceb
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:17:29 +0200

    Update skrypt06-25084.py

--- programing-exercises/skrypt06-25084.py
+++ programing-exercises/skrypt06-25084.py
@@ -2,10 +2,13 @@ import random
 import string
 
 def generate_dict():
+    # Tworzymy słownik, gdzie klucze to liczby od 10 do 20 (włącznie),
+    # a wartości to losowe 8-znakowe ciągi składające się z liter i cyfr
     wynik = {
         i: ''.join(random.choices(string.ascii_letters + string.digits, k=8))
         for i in range(10, 21)
     }
+    # Wypisujemy wygenerowany słownik
     print(wynik)
 
 if __name__ == '__main__':
