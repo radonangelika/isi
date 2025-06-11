d7a6e5f7096c2a492a5fe1e3bd23c0a6a85c7fa2
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:22:09 +0200

    Update skrypt13-25084.py

--- programing-exercises/skrypt13-25084.py
+++ programing-exercises/skrypt13-25084.py
@@ -1,6 +1,9 @@
 def skip_multiples_of_3():
+    # Iterujemy po liczbach od 1 do 50
     for i in range(1, 51):
+        # Sprawdzamy, czy liczba nie jest podzielna przez 3
         if i % 3 != 0:
+            # Wypisujemy liczbę, oddzielając je spacją (bez nowej linii)
             print(i, end=' ')
 
 if __name__ == '__main__':
