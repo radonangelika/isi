5218ae54f536b472d4ca53e18b4b691fa346df24
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:32:56 +0200

    Update skrypt26-25084.py

--- programing-exercises/skrypt26-25084.py
+++ programing-exercises/skrypt26-25084.py
@@ -1,7 +1,8 @@
-# Należało pobrać chromeDriver 
-# Używane komendy:
-#pip install selenium
-#pip freeze > requirements.txt
+# Komentarz: przed uruchomieniem upewnij się, że masz zainstalowany chromedriver 
+# i jest on zgodny z wersją Twojej przeglądarki Chrome.
+# Instalacja Selenium: pip install selenium
+# Aby zapisać aktualne zależności: pip freeze > requirements.txt
+
 import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,7 +11,6 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
 class Home():
-
     def __init__(self, header_name, price, price_m2):
         self.header_name = header_name
         self.price = price
@@ -24,17 +24,23 @@ class Home():
         }
 
 def pobranie_postow():
+    # Ustawienie usługi chromedrivera (podaj właściwą ścieżkę)
     service = Service('./chromedriver.exe')
     driver = webdriver.Chrome(service=service)
+
+    # Wejście na stronę z listą ofert mieszkań
     driver.get('https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/pomorskie/gdynia/gdynia/gdynia?priceMax=600000&viewType=listing')
 
+    # Czekamy maksymalnie 15 sekund na załadowanie co najmniej jednego artykułu z ofertą
     post_loaded = expected_conditions.presence_of_element_located((By.TAG_NAME, 'article'))
     WebDriverWait(driver, 15).until(post_loaded)
 
     slownik = {}
     i = 1
 
+    # Iterujemy po wszystkich znalezionych elementach 'article' (ofertach)
     for post in driver.find_elements(By.TAG_NAME, 'article'):
+        # Pobieramy tytuł, cenę i cenę za m2 z elementów wewnątrz artykułu
         tytul = post.find_element(By.CLASS_NAME, 'css-16vl3c1').text
         cena = post.find_element(By.CLASS_NAME, 'css-afwkhs').text
         cena_m2 = post.find_element(By.XPATH, ".//dt[text()='Cena za metr kwadratowy']/following-sibling::dd[1]/span").text
@@ -43,17 +49,17 @@ def pobranie_postow():
         slownik[f'oferta_{i}'] = home
         i += 1
 
+    # Zapis do pliku CSV z nagłówkami
     with open("home.csv", mode="w", newline="", encoding="utf-8") as plik:
         writer = csv.writer(plik)
-        writer.writerow(["header_name", "price", "price_m2"]) #Nagłówki
+        writer.writerow(["header_name", "price", "price_m2"])  # Nagłówki
         
-        #Wypisanie elementów słownika
         for key, oferta in slownik.items():
             print(key, oferta.return_data())
             writer.writerow([oferta.header_name, oferta.price, oferta.price_m2])
 
-
-    driver.stop_client()
+    # Poprawne zamknięcie sesji webdrivera
+    driver.quit()
 
 if __name__ == '__main__':
     pobranie_postow()
