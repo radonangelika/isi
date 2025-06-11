df4c522d6a3110d868dbbb213624d5c50fd8bed8
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:29:15 +0200

    Update skrypt24-25084.py

--- programing-exercises/skrypt24-25084.py
+++ programing-exercises/skrypt24-25084.py
@@ -1,9 +1,12 @@
 import csv
 
 def generuj_pc_csv(nazwa_pliku):
+    # Otwieramy plik CSV do zapisu z odpowiednim kodowaniem i bez dodatkowych pustych linii
     with open(nazwa_pliku, 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
+        # Zapisujemy nagłówki kolumn
         writer.writerow(['pc_name', 'ip'])
+        # Tworzymy 100 wpisów komputerów z nazwami P1, P2, ... P100 i adresami IP w podsieci 172.30.2.x
         for i in range(1, 101):
             ip = f"172.30.2.{i}"
             pc_name = f"P{i}"
