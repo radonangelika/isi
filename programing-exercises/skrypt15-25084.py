e2ff5c00938342bd61319b2dbec544fb6218eb8e
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:23:28 +0200

    Update skrypt15-25084.py

--- programing-exercises/skrypt15-25084.py
+++ programing-exercises/skrypt15-25084.py
@@ -1,9 +1,9 @@
 def collect_divisible():
-    liczby = []
-    for i in range(1, 101):
-        if i % 3 == 0 or i % 5 == 0:
-            liczby.append(i)
-    print(liczby)
+    liczby = []  # Inicjalizujemy pustą listę do przechowywania liczb podzielnych przez 3 lub 5
+    for i in range(1, 101):  # Przechodzimy przez liczby od 1 do 100
+        if i % 3 == 0 or i % 5 == 0:  # Sprawdzamy, czy liczba jest podzielna przez 3 lub przez 5
+            liczby.append(i)  # Jeśli tak, dodajemy ją do listy
+    print(liczby)  # Wyświetlamy listę znalezionych liczb
 
 if __name__ == '__main__':
     collect_divisible()
