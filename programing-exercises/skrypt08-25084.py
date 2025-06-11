36db1e8ac1aaad2a88d78e2b8bc449abf2b4c74f
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:18:31 +0200

    Update skrypt08-25084.py

--- programing-exercises/skrypt08-25084.py
+++ programing-exercises/skrypt08-25084.py
@@ -3,11 +3,16 @@ import string
 from collections import Counter
 
 def analyze_random_string():
+    # Generujemy losowy ciąg 100 znaków z liter (małych i wielkich) oraz cyfr
     ciag = ''.join(random.choices(string.ascii_letters + string.digits, k=100))
+    # Tworzymy licznik wystąpień poszczególnych znaków w ciągu
     licznik = Counter(ciag)
+    # Wypisujemy obiekt Counter z podsumowaniem wystąpień znaków
     print(licznik)
 
+    # Zamieniamy licznik na listę krotek (znak, liczba_wystąpień)
     lista = list(licznik.items())
+    # Wypisujemy tę listę
     print(lista)
 
 if __name__ == '__main__':
