f6edb94c2257f86fb5300dcc7785c22f5b657325
radonangelika <radonangelika1@gmail.com>
2025-06-11 14:16:11 +0200

    Update skrypt04-25084.py

--- programing-exercises/skrypt04-25084.py
+++ programing-exercises/skrypt04-25084.py
@@ -1,13 +1,20 @@
 def find_all_occurrences():
+    # Pobieramy od użytkownika tekst, w którym będziemy szukać
     tekst = input("Podaj tekst: ")
+    # Pobieramy ciąg znaków, którego będziemy szukać
     szukany = input("Podaj ciąg do wyszukania: ")
+    # Lista do przechowywania indeksów znalezionych wystąpień
     indeksy = []
 
+    # Szukamy pierwszego wystąpienia szukanego ciągu w tekście
     i = tekst.find(szukany)
+    # Pętla działa dopóki znajdziemy kolejne wystąpienia (find zwraca -1 gdy brak)
     while i != -1:
-        indeksy.append(i)
+        indeksy.append(i)  # dodajemy indeks do listy
+        # Szukamy kolejnego wystąpienia, zaczynając od pozycji po ostatnio znalezionym indeksie
         i = tekst.find(szukany, i + 1)
 
+    # Jeśli lista indeksów nie jest pusta, wypisujemy ją, w przeciwnym wypadku -1
     print(indeksy if indeksy else -1)
 
 if __name__ == '__main__':
